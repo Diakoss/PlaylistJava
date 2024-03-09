070ba19cca12c3212a98724159bf46e3ec7f8769
Diakoss <xmaximusz@outlook.com>
2024-03-08 22:44:28 -0600

    Playlist en Java Profesor Alfonso

---
+++ src/main/java/com/mycompany/playlistjava/PlaylistJava.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.playlistjava;
+
+public class PlaylistJava {
+
+    public static void main(String[] args) {
+        Musicas p1=new Musicas("What is love", "Haddaway", "1992", 4);
+        p1.ShowPlaylist();
+    }
+}
